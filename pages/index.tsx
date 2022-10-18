3488efc26d73e91e9e048e8530e17a8c54894537
Nils Weigel <nils.weigel@gmail.com>
2022-10-18 21:09:09 +0200

    removed some of the sample code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,48 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { GetStaticProps } from 'next'
+import Layout from '../components/layout'
+import {useState} from "react";
+import {Device} from "../lib/Device";
+import DeviceSelector from "../components/Device/DeviceSelector";
+import DeviceStatus from "../components/Device/DeviceStatus";
+import LedWallMode from "../components/LedWallMode";
+import LedWallPresets from "../components/LedWallPresets";
+import DeviceSystemInfo from "../components/Device/DeviceSystemInfo";
+import {GetStaticProps} from "next";
+import {getDevices} from "../lib/devices";
 
-export default function Home({
-  allPostsData
-}: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
-}) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
+export default function Home({allDevices}) {
+    const [device, setDevice] = useState<Device|null>(null);
+
+    if (device) {
+        return (
+            <Layout>
+                <Head>
+                    <title>{device.name}</title>
+                </Head>
+                <h2>{device.name}</h2>
+                <DeviceStatus device={device} />
+                <LedWallMode device={device} />
+                <LedWallPresets device={device} />
+                <DeviceSystemInfo device={device} />
+            </Layout>
+        )
+    }
+
+    return (
+        <Layout home>
+            <Head>
+                <title>Devices</title>
+            </Head>
+            <DeviceSelector devices={allDevices} selectCallback={(device: Device) => setDevice(device)} />
+        </Layout>
+    )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
+    const allDevices = getDevices()
+    return {
+        props: {
+            allDevices
+        }
     }
-  }
 }
